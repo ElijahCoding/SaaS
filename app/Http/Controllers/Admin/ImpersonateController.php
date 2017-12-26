d3673bfa6a3250c87648211566f2f6ab674b623f
Elijah <elijahsharper1@gmail.com>
2017-12-26 17:05:55 +0300

    Impersonating Start Stop

--- app/Http/Controllers/Admin/ImpersonateController.php
+++ app/Http/Controllers/Admin/ImpersonateController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ImpersonateStartRequest;
 
 class ImpersonateController extends Controller
 {
     public function index()
     {
-      dd('you are in');
+      return view('admin.impersonate.index');
+    }
+
+    public function start(ImpersonateStartRequest $request)
+    {
+      $user = User::where('email',$request->email)->first();
+
+      session()->put('impersonate', $user->id);
+
+      return redirect('/')->withSuccess("You are now impersonating {$user->name}");
+    }
+
+    public function destroy()
+    {
+      session()->forget('impersonate');
+
+      return redirect('/');
     }
 }
--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -14,6 +14,9 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      Blade::if('impersonating', function () {
+          return session()->has('impersonate');
+      });
 
       Blade::if('admin', function () {
           return auth()->user()->hasRole('admin');
